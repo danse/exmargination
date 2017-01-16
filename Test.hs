4fdc88fa2256a1f02922be52e32a3e12b7e11c91
danse <f.occhipinti@gmail.com>
2017-01-16 14:13:25 +0100

    remove logic now encapsulated in Vishnje and Collection

--- Test.hs
+++ Test.hs
@@ -1,29 +1,13 @@
 import Test.Hspec
-import Collect( convert )
 import Tags( getTags )
 import Data.ByteString.Lazy.Char8( pack )
 import Data.Aeson( eitherDecode )
 import Data.Functor( fmap )
 
--- i cannot reuse the eitherDecode, because the resulting function
--- would have the same type signature, while i need different types
--- for analyses and margins
-convertDecoded input = fmap (convert 1) (eitherDecode (pack input))
-decoded = eitherDecode . pack 
-
-runCase num = do
-  input  <- readFile $ "fixtures/"++ num ++"/input.json"
-  output <- readFile $ "fixtures/"++ num ++"/output.json"
-  (convertDecoded input) `shouldBe` (decoded output)
-
 tagCase input output = it input ((getTags input) `shouldBe` output)
 
 main :: IO ()
 main = hspec $ do
-  describe "convert" $ do
-    it "first fixture" $ runCase "1"
-    it "simple fixture" $ runCase "2"
-    it "input with a first element in wrong date order" $ runCase "3"
   describe "getTags" $ do
     tagCase "#one two #three" ["one", "three"]
     tagCase "#one #one" ["one"]
--- exmargination-collection.hs
+++ exmargination-collection.hs
@@ -1,48 +1,36 @@
 {-# LANGUAGE RecordWildCards #-}
-import Collect( convertEncode, convertEncodeWithTime )
+import CollectionGraph( collectionGraph )
 import System.Environment
 import Data.ByteString.Lazy.Char8( pack, unpack )
 import Data.Functor( fmap )
 import Data.Monoid( (<>) )
 import Control.Applicative( some )
 import Options.Applicative
-import Data.DateTime( getCurrentTime )
+import Margin
+import qualified Data.Text as T
 
 data Options = Options {
   days :: Int,
   fill :: Bool,
-  inputs :: [String],
-  output :: String
+  inputs :: [String]
   }
 
 optionParser :: Parser Options
 optionParser = Options
-               <$> option auto (long "days" <> short 'd' <> value 1)
+               <$> option auto (long "days" <> short 'd' <> Options.Applicative.value 1)
                <*> switch (long "fill" <> short 'f')
                <*> some (strOption (long "input"
                                     <> short 'i'
                                     <> metavar "INPUT_MARGIN_FILE"))
-               <*> strOption (long "output"
-                              <> short 'o'
-                              <> metavar "OUTPUT_COLLECTION_FILE")
 
 optionParserInfo :: ParserInfo Options
 optionParserInfo = info optionParser fullDesc
 
+toGraphData (Margin value desc time) = (T.pack desc, value, time)
+
 collection :: Options -> IO ()
-collection Options {..} = 
-  let convert = (convertEncode days) . (map pack)
-      convertWithTime t = (convertEncodeWithTime t days) . (map pack)
-      write = (writeFile output) . unpack
-  in do
-    strings <- sequence $ map readFile inputs
-    if fill
-      then
-      do
-        dateTime <- getCurrentTime
-        write $ convertWithTime dateTime strings
-      else
-      do
-        write $ convert strings
+collection Options {..} = do
+    margins <- getAllMargins inputs
+    (collectionGraph days . map toGraphData) margins
 
 main = execParser optionParserInfo >>= collection
